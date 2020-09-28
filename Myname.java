1665ff51d4d45d2175f27c8dbfb7cd24b9870414
Yeremmy Garcia <yeremmygarcia124@gmail.com>
2020-09-28 01:29:53 +0000

    this is my third git commit

--- Myname.java
+++ Myname.java
@@ -1,6 +1,6 @@
 public class Myname{
 	public static void main(String[] args){
-		System.out.println("My favorite programming language is java");
+		System.out.println("I am taking CS407 because I want to learn linux");
 	}
 }
 
